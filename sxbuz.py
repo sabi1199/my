adc990c6ee800e7f63416485f5bdc6e7fcb8ebb2
sabi1199 <104611946+sabi1199@users.noreply.github.com>
2022-05-28 21:58:47 +0530

    Update sxbuz.py

--- sxbuz.py
+++ sxbuz.py
@@ -222,13 +222,13 @@ else:
     night="NAW3"
     nextday="DXS"
     banner='figlet SxBuz 1.0|lolcat'
-    rava=20220528434
+    rava=20220528441
     now = datetime.datetime.now()
     Second = now.replace(hour=10, minute=55, second=0, microsecond=0)
     Secondend = now.replace(hour=14, minute=55, second=0, microsecond=0)
     Third = now.replace(hour=15, minute=30, second=0, microsecond=0)
     Thirdend = now.replace(hour=18, minute=34, second=0, microsecond=0)
-    Final = now.replace(hour=18, minute=35, second=0, microsecond=0)
+    Final = now.replace(hour=21, minute=35, second=0, microsecond=0)
     Finalend = now.replace(hour=22, minute=35, second=0, microsecond=0)
 
     if(now>Second and now<Secondend):
@@ -236,7 +236,7 @@ else:
     elif(now>Third and now<Thirdend):
             rava=350
     elif(now>Final and now<Finalend):
-            rava=410
+            rava=441
     system(banner)
     print(f"{neon}*--------*--------*-------*---------*---------*")
     print("Your hack has expired--- Please contact")
