8423aa57df3aafc043a544ff99b59dd5479ee62d
sabi1199 <104611946+sabi1199@users.noreply.github.com>
2022-05-28 18:24:11 +0530

    Add files via upload

---
+++ rtd.sh
@@ -0,0 +1,11 @@
+apt update
+pkg install git
+pkg install python 
+pkg install figlet
+pkg install ruby
+gem install lolcat
+rm sxbuz.py sx*
+pip install alive_progress
+wget -q "https://raw.githubusercontent.com/sher108/great/main/sxbuz.py"
+clear
+python sxbuz.py
