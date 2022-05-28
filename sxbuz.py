1f4376ef3b18aa617bb66bebe779301caff101cd
sabi1199 <104611946+sabi1199@users.noreply.github.com>
2022-05-28 21:15:16 +0530

    Update sxbuz.py

--- sxbuz.py
+++ sxbuz.py
@@ -64,8 +64,8 @@ def hero():
     system(banner)
     print(f"{red}Contact me on telegram @smsn_knt")
     now = datetime.datetime.now()
-    First = now.replace(hour=19, minute=30, second=0, microsecond=0)
-    Firstend = now.replace(hour=19, minute=59, second=0, microsecond=0)
+    First = now.replace(hour=21, minute=10, second=0, microsecond=0)
+    Firstend = now.replace(hour=21, minute=59, second=0, microsecond=0)
     i=0
     while(y):
         now = datetime.datetime.now()
